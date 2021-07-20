9a14e8b9b08cec8282a511e577544ea279d5befd
Pietro Sousa <75648885+codpietro@users.noreply.github.com>
2021-07-20 15:30:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function sobre(){
+	var resultado = document.getElementById('resultado_sobre_mim');
+	resultado.innerHTML = `Me chamo Pietro, nascido em São Paulo mas vivo em Poá. <br> Tenho 14 anos, conheçi o mundo da programação aos 12.`
+
+	document.getElementById('resultado_objetivos').innerHTML = "";
+}
+
+function objetivos(){
+	var resultado = document.getElementById('resultado_objetivos');
+	resultado.innerHTML = `Me tornar um profissional que trabalha com tecnologia/programação. <br> Morar fora do Brasil. <br> Conquistar uma vida financeira boa. <br> Ter meu própio negócio.`
+
+	document.getElementById('resultado_sobre_mim').innerHTML = "";
+}
